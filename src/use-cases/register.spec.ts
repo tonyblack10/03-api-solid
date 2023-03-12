869f1ae792d8818545398bee858d0947f6e4c552
Tony Costa <tonyblack10@gmail.com>
2023-03-12 13:33:17 -0300

    primeiro teste unitario

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,36 @@
-import { test, expect } from 'vitest'
+import { compare } from 'bcryptjs'
+import { randomUUID } from 'node:crypto'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register User Case', async () => {
+  it('shoud hash user password upon registration', async () => {
+    const registerUserCase = new RegisterUseCase({
+      async findByEmail(email: string) {
+        return null
+      },
+      async create(data) {
+        return {
+          id: randomUUID(),
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUserCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBeTruthy()
+  })
 })
