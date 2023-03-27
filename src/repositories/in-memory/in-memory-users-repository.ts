0e2b8a444952ceffaa6d12ebc12158a0a09f2013
Tony Costa <tonyblack10@gmail.com>
2023-03-27 00:23:10 -0300

    fix in-memory-users-repository

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,30 +1,16 @@
-import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '@/repositories/users-repository'
+import { User, Prisma, Role } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
-import { UsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
 
-  async findById(id: string): Promise<User | null> {
+  async findById(id: string) {
     const user = this.items.find((item) => item.id === id)
 
     if (!user) {
       return null
     }
-
-    return user
-  }
-
-  async create(data: Prisma.UserCreateInput) {
-    const user = {
-      id: randomUUID(),
-      name: data.name,
-      email: data.email,
-      password_hash: data.password_hash,
-      created_at: new Date(),
-    }
-
-    this.items.push(user)
 
     return user
   }
@@ -35,6 +21,21 @@ export class InMemoryUsersRepository implements UsersRepository {
     if (!user) {
       return null
     }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user: User = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      role: data.role ?? Role.MEMBER,
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
 
     return user
   }
